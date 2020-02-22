ac2dbbd783d15ce29990323df6579973d647386a
rmnbrtshk <rmnbrtshk@gmail.com>
2020-02-22 12:45:53 +0200

    init

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+
+function MyComponent ({ t }) {
+  return <h1>{t('Welcome to React')}</h1>
+}
+
+export default withTranslation()(MyComponent);
