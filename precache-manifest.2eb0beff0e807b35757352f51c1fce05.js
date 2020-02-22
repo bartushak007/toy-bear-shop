269793d895048aa7df65876c86f89f3f9a828c76
Roman <rmnbrtshk@gmail.com>
2020-02-22 18:36:52 +0200

    Updates

---
+++ precache-manifest.2eb0beff0e807b35757352f51c1fce05.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40d2b1f9e03448c01328101064e55a73",
+    "url": "/*/index.html"
+  },
+  {
+    "revision": "ef8246f35ff5f714065f",
+    "url": "/*/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6ffedef0473fa6e16e2f",
+    "url": "/*/static/css/main.c7b0d61c.chunk.css"
+  },
+  {
+    "revision": "ef8246f35ff5f714065f",
+    "url": "/*/static/js/2.d6889013.chunk.js"
+  },
+  {
+    "revision": "591f46f478d3e62205980cc9fb0c9b34",
+    "url": "/*/static/js/2.d6889013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffedef0473fa6e16e2f",
+    "url": "/*/static/js/main.b50b78de.chunk.js"
+  },
+  {
+    "revision": "7d5f127522b9cb5abe55",
+    "url": "/*/static/js/runtime-main.d9665694.js"
+  }
+]);
